2a7dbf54189c81e3bb898a0f501d9e7000eb8b45
Lukas Roberts <lukas.roberts.90@googlemail.com>
2025-03-03 09:47:31 +0100

    adding video

--- astrowind-main/src/types.d.ts
+++ astrowind-main/src/types.d.ts
@@ -276,6 +276,7 @@ export interface Steps extends Omit<Headline, 'classes'>, Widget {
 export interface Content extends Omit<Headline, 'classes'>, Widget {
   content?: string;
   image?: string | unknown;
+  videoUrl?: string | unknown;
   items?: Array<Item>;
   columns?: number;
   isReversed?: boolean;
